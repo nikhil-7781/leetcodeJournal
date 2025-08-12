c03fbd8c19ad621a354c3f6cdcc63271406640c1
nikhil-7781 <134190512+nikhil-7781@users.noreply.github.com>
2025-08-12 11:28:32 +0530

    Create Subsets.py

---
+++ Subsets/Subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+
+        sub=[]
+        def dfs(i):
+            if i>=len(nums):
+                res.append(sub.copy())
+                return
+            #include nums[i]
+            sub.append(nums[i])
+            dfs(i+1)
+
+            #exclude nums[i]
+            sub.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
+
